7efaada9cb7f4d1f9b1ec3810245a76d634fb7ab
Kirigalla <Nuotatore260104!>
2024-11-29 21:27:42 -0600

    comentarios para version final

---
+++ src/Clases/Bot.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import Clases.Consulta;
+
+/**
+ *
+ * @author Alumno11
+ */
+public class Bot extends Consulta {
+    Consulta c1 = new Consulta("mensjae");
+
+    public Bot(String mensaje) {
+        super(mensaje);
+    }
+    
+    
+    public void eliminarSignos(Consulta c1){
+    
+    }
+    
+    
+}
